415f87918f0f0a9c5fd14ae63ac86db8f44fe8d9
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2021-02-23 19:58:12 +0530

    BACKEND UPDATED

--- models/category.js
+++ models/category.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema(
   {
-    name: {
+    // material type --> Aluminium, Plastic, Steel
+    type: {
       type: String,
       trim: true,
       required: true,
--- models/product.js
+++ models/product.js
@@ -3,42 +3,71 @@ const { ObjectId } = mongoose.Schema;
 
 const productSchema = new mongoose.Schema(
   {
-    name: {
+    // material shape --> circle, square, triangle
+    shape: {
       type: String,
       trim: true,
       required: true,
-      maxlength: 32
+      maxlength: 32,
     },
-    description: {
+    color: {
       type: String,
       trim: true,
       required: true,
-      maxlength: 2000
+      maxlength: 2000,
     },
-    price: {
+    length: {
       type: Number,
       required: true,
       maxlength: 32,
-      trim: true
+      trim: true,
+    },
+    width: {
+      type: Number,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    text: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
     },
     category: {
       type: ObjectId,
-      ref: "Category",
-      required: true
+      ref: 'Category',
+      required: true,
     },
     stock: {
-      type: Number
+      type: Number,
     },
     sold: {
       type: Number,
-      default: 0
+      default: 0,
+    },
+    materialCost: {
+      type: Number,
+      default: 0,
+    },
+    setupCost: {
+      type: Number,
+      default: 0,
+    },
+    jobCost: {
+      type: Number,
+      default: 0,
+    },
+    totalCost: {
+      type: Number,
+      default: 0,
     },
     photo: {
       data: Buffer,
-      contentType: String
-    }
+      contentType: String,
+    },
   },
   { timestamps: true }
-);
+)
 
 module.exports = mongoose.model("Product", productSchema);
